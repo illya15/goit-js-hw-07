b6ac39f3bbaf00df9c876ebde1fd72dce5e4f745
illya15 <113913094+illya15@users.noreply.github.com>
2023-03-04 22:28:00 +0200

    hw7task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = galleryItems.map(({ original, preview, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}).join("");
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
 
 console.log(galleryItems);
